6d3a81a21973ffdf340d79377ef2c69fbdfb05f6
kelvinnmuia <kelvinnmuia@gmail.com>
2026-06-03 17:18:08 +0300

    created a post method for fastapi

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -9,3 +10,8 @@ async def root():
 @app.get("/posts")
 async def get_posts():
     return {"data": "This is your posts"}
+
+@app.post("/createpost")
+def create_post(payload: dict = Body(...)):
+    print(payload)
+    return {"newpost": f"title: {payload['title']} content: {payload['content']}"}
