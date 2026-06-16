4dba9cca604d4bfcdea3690c496563ae6cdf1dff
kelvinnmuia <kelvinnmuia@gmail.com>
2026-06-16 15:43:03 +0300

    fixed the dotenv import error in config file

--- app/config.py
+++ app/config.py
@@ -1,4 +1,8 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent
+ENV_PATH = BASE_DIR / ".env"
 
 class Settings(BaseSettings):
     database_hostname: str
@@ -11,6 +15,6 @@ class Settings(BaseSettings):
     access_token_expire_time: int
 
     class Config:
-        model_config = SettingsConfigDict(env_file=".env")
+        model_config = SettingsConfigDict(env_file=ENV_PATH)
 
 settings = Settings()
